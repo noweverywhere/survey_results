faf3f965197c6ac07c94a80322a34593a5bee768
noweverywhere <marinus@noweverywhere.ca>
2018-01-17 02:06:41 +1000

    Add route that serves the index.json I was given

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const app = express()
 const APP_PORT = parseInt(process.env.SURVEY_APP_PORT) || 3001
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
 
-app.get('/', (req, res) => res.send('Hello Culture Amp'))
+const app = express()
+const router = express.Router()
+
+router.get('/api/v1', (req, res) => res.sendFile(path.join(__dirname, 'surveys', 'index.json')))
+router.use(serveStatic(path.join(__dirname, 'public')))
+app.use('/', router)
 
 app.listen(APP_PORT, () => console.log(`survey results api now listening on port ${APP_PORT}`))
