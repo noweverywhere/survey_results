af252bb9de54e0a7eeaf7be526121a18bdb669dc
noweverywhere <marinus@noweverywhere.ca>
2018-01-18 16:01:26 +1000

    Serve survey_results files from API as well

--- api/index.js
+++ api/index.js
@@ -5,9 +5,22 @@ const serveStatic = require('serve-static')
 
 const app = express()
 const router = express.Router()
+const ROOT_PATH = '/api/v1'
 
-router.get('/api/v1', (req, res) => res.sendFile(path.join(__dirname, 'surveys', 'index.json')))
+router.get(
+  `${ROOT_PATH}*`,
+  (req, res) => {
+    const requestPath = req.path.substring(ROOT_PATH.length)
+    return res.sendFile(
+      path.join(__dirname, 'surveys', requestPath || 'index.json'),
+      (err) => { err && res.sendStatus(404) }
+    )
+  }
+)
 router.use(serveStatic(path.join(__dirname, 'public')))
 app.use('/', router)
 
-app.listen(APP_PORT, () => console.log(`survey results api now listening on port ${APP_PORT}`))
+app.listen(
+  APP_PORT,
+  () => console.log(`survey results api now listening on port ${APP_PORT}`)
+)
