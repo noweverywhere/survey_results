728f646ee8f3c33f7bc99bd930b912c1a83553b8
noweverywhere <marinus@noweverywhere.ca>
2018-01-22 15:58:23 +1000

    Make a seperate function for fetch request in api lib for reuse later

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,28 +1,32 @@
 import fetch from 'isomorphic-fetch'
 
+export const get = ({url}) => {
+  return fetch(url)
+    .then(response => {
+      return response.json().then(
+        (data) => ({
+          data,
+          success: true
+        })
+      )
+    })
+    .catch((error) => ({
+        success: false,
+      })
+    )
+}
+
 export const surveys = {
   index: ({
     repositoryUrl,
     success
   }) => {
-    const apiUrl = (
+    const url = (
       repositoryUrl ||
       process.env.API_BASE_URL ||
       `${process.env.PUBLIC_URL}/surveys/index.json`
     )
 
-    return fetch(repositoryUrl || apiUrl)
-      .then(response => {
-        return response.json().then(
-          (data) => ({
-            data,
-            success: true
-          })
-        )
-      })
-      .catch((error) => ({
-          success: false,
-        })
-      )
+    return get({ url })
   }
 }
