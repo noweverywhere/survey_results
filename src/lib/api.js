14c0ba4f84bf7a13ffa143ef578d6e35aac8b36e
noweverywhere <marinus@noweverywhere.ca>
2018-01-27 19:00:27 +1000

    Add basic api library request json files from API

---
+++ src/lib/api.js
@@ -0,0 +1,53 @@
+import fetch from 'isomorphic-fetch'
+import {
+  RESPONSE_NOT_OK,
+  BAD_FORMAT,
+  CONNECTIVITY_PROBLEMS
+} from './constants'
+
+const resolveWithErrors = (response, errorMessage, error = null) => (
+  Promise.resolve({
+    data: null,
+    statusCode: response.status || null,
+    errorMessage: errorMessage,
+    success: false,
+    error: (error && error.toString()) || null
+  })
+)
+
+export const get = (url) => {
+  return (
+    fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        return resolveWithErrors(response, RESPONSE_NOT_OK)
+      }
+      return response.json().then((data) => {
+        return Promise.resolve({
+          statusCode: response.status,
+          errorMessage: null,
+          data,
+          success: true
+        })
+      })
+      .catch((error) => {
+        return resolveWithErrors(response, BAD_FORMAT, error)
+      })
+    })
+    .catch((error) => {
+      return resolveWithErrors({}, CONNECTIVITY_PROBLEMS, error)
+    })
+  )
+}
+
+export const api = (baseUrl) => ({
+  index: () => {
+    return get(`${baseUrl}/index.json`).then((outcome) => {
+      return Object.assign(
+        outcome, {data: (outcome.data && outcome.data.survey_results)}
+      )
+    })
+  }
+})
+
+export default { init: api }
---
+++ src/lib/api.test.js
@@ -0,0 +1,91 @@
+import nock from 'nock'
+import { get, api } from './api'
+import {
+  RESPONSE_NOT_OK,
+  BAD_FORMAT,
+  CONNECTIVITY_PROBLEMS
+} from './constants'
+
+describe('lib/api', () => {
+  const testApiBaseUrl = 'https://api.example.com'
+
+  describe('get', () => {
+    it('resolves object with expected shape during success', () => {
+      nock(testApiBaseUrl)
+       .get('/')
+       .reply(200, {foo: 'bar'})
+
+      const promise = get(testApiBaseUrl)
+
+      return expect(promise).resolves.toEqual({
+        data: {foo: 'bar'},
+        statusCode: 200,
+        errorMessage: null,
+        success: true
+      })
+    })
+
+    describe('failure', () => {
+      it('resolves object with expected shape during failure', () => {
+        nock(testApiBaseUrl)
+         .get('/')
+         .reply(400, {foo: 'bar'})
+
+        const promise = get(testApiBaseUrl)
+
+        return expect(promise).resolves.toEqual({
+          data: null,
+          statusCode: 400,
+          errorMessage: RESPONSE_NOT_OK,
+          success: false,
+          error: null
+        })
+      })
+
+      it('resolves with expected shape output from bad data', () => {
+        nock(testApiBaseUrl)
+         .get('/')
+         .reply(200, 'Not json')
+
+        const promise = get(testApiBaseUrl)
+
+        return expect(promise).resolves.toEqual({
+          data: null,
+          errorMessage: BAD_FORMAT,
+          statusCode: 200,
+          success: false,
+          error: `FetchError: invalid json response body at ${testApiBaseUrl} reason: Unexpected token N in JSON at position 0`
+        })
+      })
+
+      it('resolves with expected shape output from serious network issues', () => {
+        const promise = get('not an url')
+
+        return expect(promise).resolves.toEqual({
+          data: null,
+          errorMessage: CONNECTIVITY_PROBLEMS,
+          statusCode: null,
+          success: false,
+          error: 'Error: only absolute urls are supported'
+        })
+      })
+    })
+  })
+
+  describe('api().index', () => {
+    it('appends the path to the surveys index', () => {
+      nock(testApiBaseUrl)
+       .get('/index.json')
+       .reply(200, {survey_results: 'bar'})
+
+      const promise = api(testApiBaseUrl).index()
+
+      return expect(promise).resolves.toEqual({
+        data: 'bar',
+        statusCode: 200,
+        errorMessage: null,
+        success: true
+      })
+    })
+  })
+})
