f754ae693d634dfb72bcffa88259a8541dea0dfe
noweverywhere <marinus@noweverywhere.ca>
2018-01-29 02:09:27 +1000

    Add API_BASE_URL environment variable
    Defaults to https://example.com for testing and http://localhost:8080
    for development. One will have to be set manually for 'npm run build'

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
@@ -14,6 +15,7 @@ module.exports = {
       { from: 'public' },
       { from: 'src/index.html' },
       { from: 'src/styles.css' }
-    ])
+    ]),
+    new webpack.EnvironmentPlugin({'API_BASE_URL': 'http://example.com'})
   ]
 }
